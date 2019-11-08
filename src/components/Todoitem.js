fc89e2c842aacd17317f62d782005358a0d3a808
thegioicuabao <vickyvuvo@gmail.com>
2019-11-08 22:18:38 +0700

    check-button

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import './Todoitem.css';
 import classNames from 'classnames';
-
+import checkImg from '../image/check.svg'
+import checkCompleteImg from '../image/check-complete.svg'
 class TodoItem extends Component{
-
   render(){
     const { item, onClick } = this.props;
+    let url = checkImg;
+    if(item.isComplete)
+      url = checkCompleteImg;
     return (  
-      <div onClick={onClick} className={classNames('TodoItem', {
+      <div className={classNames('TodoItem', {
         'TodoItem-complete': item.isComplete
       })}>
+        <img src={url} width={24} height={24} onClick={onClick}/>
         <p>{this.props.item.title}</p>
       </div>
     )
