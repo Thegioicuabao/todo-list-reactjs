b76a03ebd6791827a9ff145161a1555e551d37d8
thegioicuabao <vickyvuvo@gmail.com>
2019-11-09 00:14:15 +0700

    Feature-Add-new-item

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 import TodoItem from './components/Todoitem'
-
+import checkAll from './image/check-all.svg'
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
+      newItem: "",
       todoItems: [
         { title: 'Mua bim bim', isComplete: true }, 
         { title: 'đi chợ', isComplete: true }, 
         { title: 'đi đá bóng', isComplete: false }
       ]
     }
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
   
   onItemClicked(item) {
@@ -32,13 +35,44 @@ class App extends React.Component {
     }
    
   }
+  onKeyUp(event) {
+    if (event.keyCode === 13) {
+      let text =  event.target.value;
+      if(!text) {
+       return;
+      }
+      text = text.trim();
+      if(!text) {
+        return;
+       }
+      this.setState({
+        newItem: "",
+        todoItems: [
+          { title: text, isComplete: false},
+          ...this.state.todoItems
+        ]
+      })
+    }
+  }
+  onChange(event) {
+    this.setState({
+      newItem: event.target.value
+    })
+  }
   render() {
-    const { todoItems } = this.state;
+    const { todoItems, newItem } = this.state;
     if(todoItems.length)
     return (
       <div className="App">
         <header className="App-header">
           <h1>todos</h1>
+          <img src={checkAll} width={24} height={24}/>
+          <input 
+            type="text" 
+            placeholder="Add a new item"
+            value = {newItem}
+            onChange = {this.onChange} 
+            onKeyUp={this.onKeyUp}/>
         </header>
         {
           todoItems.length && todoItems.map((item, index) => 
