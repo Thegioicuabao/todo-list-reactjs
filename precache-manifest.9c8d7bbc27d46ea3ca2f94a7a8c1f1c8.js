d1f6ecc3b9bc210854816dd13e25b641b1ef47fd
thegioicuabao <vickyvuvo@gmail.com>
2019-11-14 20:03:12 +0700

    Updates

---
+++ precache-manifest.9c8d7bbc27d46ea3ca2f94a7a8c1f1c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89bf6ea62f0adef39212777e550f0bb6",
+    "url": "thegioicuabao.github.io/todo-list-reactjs/index.html"
+  },
+  {
+    "revision": "c277deae36679418d110",
+    "url": "thegioicuabao.github.io/todo-list-reactjs/static/css/main.f59e2a33.chunk.css"
+  },
+  {
+    "revision": "ac9455eecc6bbaa7cb3b",
+    "url": "thegioicuabao.github.io/todo-list-reactjs/static/js/2.31b72c3b.chunk.js"
+  },
+  {
+    "revision": "c277deae36679418d110",
+    "url": "thegioicuabao.github.io/todo-list-reactjs/static/js/main.bfe15e37.chunk.js"
+  },
+  {
+    "revision": "97b6d3a81b8138f38c02",
+    "url": "thegioicuabao.github.io/todo-list-reactjs/static/js/runtime-main.2563e511.js"
+  },
+  {
+    "revision": "bdae0272aebe7058db64bd91a1f8524a",
+    "url": "thegioicuabao.github.io/todo-list-reactjs/static/media/check-all.bdae0272.svg"
+  },
+  {
+    "revision": "c884f427a7f5b780545f8a264cdc8688",
+    "url": "thegioicuabao.github.io/todo-list-reactjs/static/media/check-complete.c884f427.svg"
+  },
+  {
+    "revision": "b191d3b7cc76b5d91c1ee961ff5e30ac",
+    "url": "thegioicuabao.github.io/todo-list-reactjs/static/media/check.b191d3b7.svg"
+  }
+]);
